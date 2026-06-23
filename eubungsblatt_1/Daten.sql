1377b0573f2acb8bcc010cecd786fb24cb85e6c0
Eugen <EOberst@cdtnue.onmicrosoft.com>
2026-06-23 15:33:24 +0200

    Daten einfügen

---
+++ eubungsblatt_1/Daten.sql
@@ -0,0 +1,27 @@
+INSERT INTO abteilung VALUES
+('Personal'),
+('Vertrieb'),
+('Buchhaltung'),
+('IT		 '),
+('Entwicklung'),
+('Produktion '),
+('Einkauf	 '),
+('Logistik	 '),
+('Verwaltung ');
+
+INSERT INTO 
+
+
+
+
+
+('Fahrer', 'Bob', 'Mozartstr. 7', '90768', 'Fuerth'	02.11.1972	eingetr. Lebensgemein-schaft 	'7', Vollzeit 
+('Platz', 'Alexander', 'Lessingstr. 72', '91054', 'Erlangen' 01.08.1955	verheiratet 	'7', Vollzeit 
+('Kreuz', 'Andreas', 'Ringstr. 40',	'90765', 'Fuerth'	26.04.1974	3', Vertrieb 	HomeOffice 
+('Geber', 'Ann', 'Schillerstr. 25',	'90762', 'Fuerth'	17.03.1987	3', Verwaltung 	Vollzeit 
+('Zug', 'Ann', 'Lindenweg 12', '90411', 'Nuernberg' 	20.03.1996	eingetr. Lebensgemein-schaft 	Vertrieb 	Minijob 
+('Bolika', 'Anna', 'Bahnhofstr. 142', '90425', 'Nuernberg' 	20.04.1981	eingetr. Lebensgemein-schaft 	Entwicklung 	Vollzeit 
+('Nass', 'Anna', 'Dorfstr. 18',	'90409', 'Nuernberg' 	10.02.1959	verheiratet 	'5', HomeOffice 
+('Theke', 'Anna', 'Kirchplatz 60', '91052', 'Erlangen'	23.01.1964	verwitwet 	'5', Teilzeit 
+('Höhle', 'Axel', 'Mühlgasse 94', '90766', 'Fuerth '	04.08.1969	3', Entwicklung 	Vollzeit 
+('Diener', 'Bernhard', 'Mühlgasse 69 '90763', 'Fuerth '	28.05.1961	eingetr. Lebensgemein-schaft 	Marketing 	Vollzeit 
